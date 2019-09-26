2678277f4e0db993618cdd899756a492289507ae
Mirrazibrahim <55575435+Mirrazibrahim@users.noreply.github.com>
2019-09-26 13:13:54 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118056/latihan16/penugasan/PBOIF210118056Latihan16Penugasan.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118056.latihan16.penugasan;
+
+/**
+ *
+ * @author 
+ * NAMA     : Mirrazibrahim
+ * KELAS    : IF-2
+ * NIM      : 10118056
+ * Deskripsi Program : Program ini berisi program untuk menampilkan Penugasan
+ */
+public class PBOIF210118056Latihan16Penugasan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Nilai c = " + c+"\n");
+        
+        c = a + b;
+        System.out.println("c = a + b = " + c);
+        c += a;
+        System.out.println("c += a = " + c);
+        c -= a;
+        System.out.println("c -= a = " + c);
+        c *= a;
+        System.out.println("c *= a = " + c);
+        a = 10;
+        c = 15;
+        c /= a;
+        System.out.println("c /= a =" + c);
+    }
+    
+}
